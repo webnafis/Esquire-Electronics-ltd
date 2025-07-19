c555a65644540977fc9cf5fe021ded21ddad0391
webnafis <shahriar.nafis210@gmail.com>
2025-07-20 01:52:48 +0600

    type animation later

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -8,31 +8,14 @@ import { Component, HostListener, OnInit } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
   public isOpenBrands = false;
   public isOpenService = false;
   public isScrolled = false;
   private scrollThreshold = 20;
-  public placeholderText = 'Search products.....';
-  public displayedText = '';
-  private currentIndex = 0;
-  public isDeleting = false;
-  private typingSpeed = 100; // ms per character
-  private timeoutId: any;
-  public isFocused = false;
-  ngOnInit() {
-    this.startTyping();
-  }
 
-  ngOnDestroy() {
-    this.clearTimer();
-  }
+
   /**
-   * onFocus()
-   * onBlur()
-   * resetAnimation()
-   * clearTimer()
-   * startTyping()
    * toggleDropdown1()
    * toggleDropdown2()
    * OnClickOutside()
@@ -40,56 +23,6 @@ export class HeaderComponent implements OnInit {
   */
 
 
-  public onFocus() {
-    this.isFocused = true;
-    this.resetAnimation();
-  }
-
-  public onBlur() {
-    this.isFocused = false;
-    // Only restart if input is empty
-    if (!this.displayedText) {
-      this.resetAnimation();
-    }
-  }
-
-  private resetAnimation() {
-    this.clearTimer();
-    this.displayedText = '';
-    this.currentIndex = 0;
-    this.isDeleting = false;
-    this.startTyping();
-  }
-
-  private clearTimer() {
-    if (this.timeoutId) {
-      clearTimeout(this.timeoutId);
-      this.timeoutId = null;
-    }
-  }
-
-  private startTyping() {
-    if (this.isFocused) return; // Don't animate while focused
-
-    const currentText = this.isDeleting
-      ? this.placeholderText.substring(0, this.currentIndex - 1)
-      : this.placeholderText.substring(0, this.currentIndex + 1);
-
-    this.displayedText = currentText;
-    this.currentIndex = this.isDeleting ? this.currentIndex - 1 : this.currentIndex + 1;
-
-    if (!this.isDeleting && currentText === this.placeholderText) {
-      this.timeoutId = setTimeout(() => {
-        this.isDeleting = true;
-        this.startTyping();
-      }, 1000);
-    } else if (this.isDeleting && currentText === '') {
-      this.isDeleting = false;
-      this.timeoutId = setTimeout(() => this.startTyping(), this.typingSpeed);
-    } else {
-      this.timeoutId = setTimeout(() => this.startTyping(), this.typingSpeed);
-    }
-  }
 
 
   public toggleDropdown1() {
