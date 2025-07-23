b7df25d23541286d127ecd546ba37f05e406c34d
webnafis <shahriar.nafis210@gmail.com>
2025-07-23 13:08:02 +0600

    swiper added to  support or service section

--- src/app/page/home/support/support.component.ts
+++ src/app/page/home/support/support.component.ts
@@ -1,11 +1,51 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, Input } from '@angular/core';
 import { FEST_PRODUCTS_DB } from '../../../DB/fest.db';
+import { SwiperContainer } from 'swiper/element';
+import { SwiperOptions } from 'swiper/types';
 
 @Component({
   selector: 'app-support',
   templateUrl: './support.component.html',
   styleUrl: './support.component.scss'
 })
-export class SupportComponent {
+export class SupportComponent implements AfterViewInit {
   public Support_products: any = FEST_PRODUCTS_DB;
+
+  ngAfterViewInit() {
+
+
+    // Register Swiper custom elements
+    // register();
+
+    // Initialize Swiper
+    const swiperElSupport = document.querySelector(`#support`) as SwiperContainer;
+    if (!swiperElSupport) return;
+    const swiperParamsSupport: SwiperOptions = {
+      loop: false,
+      slidesPerView: 6,
+      spaceBetween: 10,
+      speed: 600, // Transition duration in ms (400-800 is smooth)
+      effect: 'slide', // Use 'fade' for fade transitions
+      // grabCursor: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: true,
+      },
+      navigation: {
+        nextEl: `.first-support`,
+        prevEl: `.last-support`,
+      },
+      on: {
+        init() {
+          // Additional initialization if needed
+        },
+      },
+    };
+
+    // Assign parameters to the Swiper element
+    Object.assign(swiperElSupport, swiperParamsSupport);
+
+    // Initialize Swiper
+    swiperElSupport.initialize();
+  }
 }
